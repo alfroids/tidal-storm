6c3b0ba3959dc808db623db4cd258291dd367ac7
Alfredo Reis <alf.reismarin@gmail.com>
2024-09-08 19:09:02 -0300

    Create Boat scene and Delete HookSpawner scene

--- scenes/hook/hook.gd
+++ scenes/hook/hook.gd
@@ -2,16 +2,35 @@ class_name Hook
 extends Area2D
 
 
+signal pulled()
+
 const SPEED: float = 400.0
 
-var target_depth: float = 0.0
+var target_depth: float = 800.0
+
+@onready var pull_timer: Timer = $PullTimer as Timer
 
 
 func _ready() -> void:
 	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(
 		self,
-		^"global_position",
-		Vector2(global_position.x, target_depth),
-		target_depth / SPEED
-	).set_trans(Tween.TRANS_SPRING).set_ease(Tween.EASE_OUT)
+		^"position",
+		target_depth * Vector2.DOWN,
+		target_depth / SPEED,
+	).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	tween.finished.connect(pull_timer.start)
+
+
+func _on_pull_timer_timeout() -> void:
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(
+		self,
+		^"position",
+		Vector2.ZERO,
+		target_depth / SPEED,
+	).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
+	tween.finished.connect(func():
+		pulled.emit()
+		queue_free()
+	)
