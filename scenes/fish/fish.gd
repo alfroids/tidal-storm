4694aa1dd9437c229a4abb58860289d699f64fdf
Alfredo Reis <alf.reismarin@gmail.com>
2024-09-08 15:21:36 -0300

    Implement smooth movement

--- scenes/fish/fish.gd
+++ scenes/fish/fish.gd
@@ -2,14 +2,27 @@ class_name Fish
 extends CharacterBody2D
 
 
-const MOVE_SPEED: float = 400.0
+
+const MAX_SPEED: float = 400.0
+const ACCELARTION: float = 1500.0
+const DRAG: float = 600.0
+
+@onready var move_speed: float = 0.0
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var move_x: float = Input.get_action_strength(&"move_right") - Input.get_action_strength(&"move_left")
 	var move_y: float = Input.get_action_strength(&"move_down") - Input.get_action_strength(&"move_up")
 	var move_dir: Vector2 = Vector2(move_x, move_y).normalized()
 
-	velocity = MOVE_SPEED * move_dir
+	if not move_dir:
+		var drag: float = delta * DRAG
+		if velocity.length_squared() > drag**2:
+			velocity -= drag * velocity.normalized()
+		else:
+			velocity = Vector2.ZERO
+	else:
+		velocity += delta * ACCELARTION * move_dir
+		velocity = velocity.limit_length(MAX_SPEED)
 
 	move_and_slide()
