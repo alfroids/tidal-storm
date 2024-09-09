8db14faffb5d3562252072fd42abaef38d039fdf
chpassos <crlshnrqpassos@gmail.com>
2024-09-09 18:48:56 -0300

    Improvements on the calm/storm cycle

--- scenes/levels/main.gd
+++ scenes/levels/main.gd
@@ -2,11 +2,21 @@ extends Node2D
 
 @export var _calm_timer: Timer
 @export var _storm_timer: Timer
+@export var _canvas_modulate: CanvasModulate
+
+func _ready():
+	_canvas_modulate.color = Color("#bcbcbc")
 
 func _on_calm_timer_timeout():
 	_storm_timer.start()
-	print("Hora da tempestade")
+	
+	# Tween para mudar a aparência do jogo p/ tempestade
+	var tween = get_tree().create_tween()
+	tween.tween_property(_canvas_modulate, "color", Color("#101010"), 1)
 
 func _on_storm_timer_timeout():
 	_calm_timer.start()
-	print("Hora da Calmaria")
+	
+	# Tween para mudar a aparência do jogo p/ calmaria
+	var tween = get_tree().create_tween()
+	tween.tween_property(_canvas_modulate, "color", Color("#bcbcbc"), 1)
