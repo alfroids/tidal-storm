6d4f60bcce4270c41c2d0463758f8f8e0911aaae
chpassos <crlshnrqpassos@gmail.com>
2024-09-10 19:12:57 -0300

    Some light orbs added

--- scenes/levels/main.gd
+++ scenes/levels/main.gd
@@ -5,18 +5,18 @@ extends Node2D
 @export var _canvas_modulate: CanvasModulate
 
 func _ready():
-	_canvas_modulate.color = Color("#bcbcbc")
+	_canvas_modulate.color = Color("#6f8695")
 
 func _on_calm_timer_timeout():
 	_storm_timer.start()
 
 	# Tween para mudar a aparência do jogo p/ tempestade
 	var tween = create_tween()
-	tween.tween_property(_canvas_modulate, "color", Color("#0c0b2b"), 1)
+	tween.tween_property(_canvas_modulate, "color", Color("#383961"), 1)
 
 func _on_storm_timer_timeout():
 	_calm_timer.start()
 
 	# Tween para mudar a aparência do jogo p/ calmaria
 	var tween = create_tween()
-	tween.tween_property(_canvas_modulate, "color", Color("#bcbcbc"), 1)
+	tween.tween_property(_canvas_modulate, "color", Color("#6f8695"), 1)
