112ad9ce4ce07ba175cad149714d5f9adae6a197
chpassos <crlshnrqpassos@gmail.com>
2024-09-11 19:10:42 -0300

    ProgressBar now represent Calm Timer left in percentage

--- scenes/UI/ui.gd
+++ scenes/UI/ui.gd
@@ -5,10 +5,5 @@ extends CanvasLayer
 
 @export var _progress_bar: ProgressBar
 
-
-
 func _process(_delta):
-	
-	
-	
-	_progress_bar.value = calm_timer.time_left
+	_progress_bar.value = (calm_timer.time_left / calm_timer.wait_time) * 100
