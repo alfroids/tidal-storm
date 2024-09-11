fa962ecc853fdafd00cf9d7b3ae036bc3d80c741
chpassos <crlshnrqpassos@gmail.com>
2024-09-11 19:03:24 -0300

    UI scene created

---
+++ scenes/UI/ui.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+@onready var calm_timer = CycleManager.calm_timer
+@onready var storm_timer = CycleManager.storm_timer
+
+@export var _progress_bar: ProgressBar
+
+
+
+func _process(_delta):
+	
+	
+	
+	_progress_bar.value = calm_timer.time_left
