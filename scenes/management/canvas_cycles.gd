de9c47fe9cda37499d13580052d198a832ce191e
chpassos <crlshnrqpassos@gmail.com>
2024-09-10 23:01:27 -0300

    Cycle autoload and the scene that will handle all changes are... connected?

--- scenes/management/canvas_cycles.gd
+++ scenes/management/canvas_cycles.gd
@@ -4,26 +4,11 @@ extends Node2D
 
 func _ready():
 	_canvas_modulate.color = Color("#6f8695") # initial Canvas color
+	CycleManager.cycle_ended.connect(_canvas_change)
 
-func _process(delta):
-	#CycleManager.calm_cycle_ended.connect() # Capturing when calm ended
-	pass
-
-
-
-#func _ready():
-	#_canvas_modulate.color = Color("#6f8695")
-#
-#func _on_calm_timer_timeout():
-	#_storm_timer.start()
-#
-	## Tween para mudar a aparência do jogo p/ tempestade
-	#var tween = create_tween()
-	#tween.tween_property(_canvas_modulate, "color", Color("#383961"), 1)
-#
-#func _on_storm_timer_timeout():
-	#_calm_timer.start()
-#
-	## Tween para mudar a aparência do jogo p/ calmaria
-	#var tween = create_tween()
-	#tween.tween_property(_canvas_modulate, "color", Color("#6f8695"), 1)
+func _canvas_change(type):
+	if type == "calm":
+		print("vai mudar pra claro")
+	elif type == "storm":
+		print("vai mudar pra escuro")
+	
--- scenes/management/cycle_manager.gd
+++ scenes/management/cycle_manager.gd
@@ -1,9 +1,16 @@
 extends Node
 
 #signal calm_cycle_started()
-signal calm_cycle_ended()
+#signal calm_cycle_ended()
 #signal storm_cycle_started()
-signal storm_cycle_ended()
+#signal storm_cycle_ended()
+
+signal cycle_start(type)
+
+
+# CalmTimer tá em autostart. Talvez seja necessário startar 
+# Só quando chegar na cena do jogo de fato.
+# teria q capturar qual a cena atual, talvez
 
 @export var calm_timer: Timer
 @export var storm_timer: Timer
@@ -11,13 +18,12 @@ signal storm_cycle_ended()
 func _on_calm_timer_timeout():
 	print("calm ended")
 	storm_timer.start()
-	calm_cycle_ended.emit()
-	
+	cycle_start.emit("storm") # tipo de ciclo que vai começar
 
 func _on_storm_timer_timeout():
 	print("storm ended")
 	calm_timer.start()
-	storm_cycle_ended.emit()
+	cycle_start.emit("calm")
 
 #@onready var calm_timer: Timer = $CalmTimer as Timer
 #@onready var storm_timer: Timer = $StormTimer as Timer
