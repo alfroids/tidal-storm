d4fd02cbf320008b22beedb5fd1d08cfe9125825
Alfredo Reis <alf.reismarin@gmail.com>
2024-09-11 21:17:23 -0300

    Update MissionItem

--- scenes/management/cycle_manager.gd
+++ scenes/management/cycle_manager.gd
@@ -8,18 +8,27 @@ enum PHASE {CALM, STORM}
 @export var calm_timer: Timer
 @export var storm_timer: Timer
 
+@onready var current_phase: PHASE = PHASE.CALM
+
 
 func _on_calm_timer_timeout() -> void:
 	#print("calm ended")
 	storm_timer.start()
+	current_phase = PHASE.STORM
 	cycle_started.emit(PHASE.STORM)
 
 
 func _on_storm_timer_timeout() -> void:
 	#print("storm ended")
 	calm_timer.start()
+	current_phase = PHASE.CALM
 	cycle_started.emit(PHASE.CALM)
 
 
-func delay_calm_timer(delay: float) -> void:
-	calm_timer.start(calm_timer.time_left + delay)
+func delay_cycle_timer(delay: float) -> void:
+	if current_phase == PHASE.STORM:
+		storm_timer.start(storm_timer.time_left + delay)
+	elif current_phase == PHASE.CALM:
+		calm_timer.start(calm_timer.time_left + delay)
+	else:
+		print("ue")
--- scenes/mission/mission-item/mission_item.gd
+++ scenes/mission/mission-item/mission_item.gd
@@ -11,5 +11,5 @@ func _ready() -> void:
 
 
 func _on_body_entered(_body: Node2D) -> void:
-	CycleManager.delay_calm_timer(data.delay_time)
+	CycleManager.delay_cycle_timer(data.delay_time)
 	queue_free()
