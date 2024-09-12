f9e6a5f8868f25ed53bf5893ad918f782c0686e8
chpassos <crlshnrqpassos@gmail.com>
2024-09-12 14:54:15 -0300

    ProgressBar changes fill color depending on the current cycle

--- scenes/UI/ui.gd
+++ scenes/UI/ui.gd
@@ -13,7 +13,12 @@ extends CanvasLayer
 
 func _process(_delta):
 	if calm_timer.time_left > 0:
+		_progress_bar.get_theme_stylebox("fill").bg_color = Color("#539987")
 		_progress_bar.value = (calm_timer.time_left / calm_max_value) * 100
 	
 	if storm_timer.time_left > 0:
+		_progress_bar.get_theme_stylebox("fill").bg_color = Color("#a30b37")
 		_progress_bar.value = (1 - storm_timer.time_left / storm_max_value) * 100
+
+ #styleBox = $ProgressBar.get("custom_styles/fg")
+	#styleBox.bg_color = Color(r, g, 0)
