5dd5e51550ad996e78f7cb30921e48286788e61e
chpassos <crlshnrqpassos@gmail.com>
2024-09-12 09:08:08 -0300

    Collecting itens increments only a portion of the ProgressBar

--- scenes/UI/ui.gd
+++ scenes/UI/ui.gd
@@ -1,13 +1,19 @@
 extends CanvasLayer
 
-@onready var calm_timer = CycleManager.calm_timer
-@onready var storm_timer = CycleManager.storm_timer
+
 
 @export var _progress_bar: ProgressBar
 
+
+@onready var calm_timer = CycleManager.calm_timer
+@onready var storm_timer = CycleManager.storm_timer
+
+@onready var calm_max_value = calm_timer.wait_time
+@onready var storm_max_value = storm_timer.wait_time
+
 func _process(_delta):
 	if calm_timer.time_left > 0:
-		_progress_bar.value = (calm_timer.time_left / calm_timer.wait_time) * 100
+		_progress_bar.value = (calm_timer.time_left / calm_max_value) * 100
 	
 	if storm_timer.time_left > 0:
-		_progress_bar.value = (1 - storm_timer.time_left / storm_timer.wait_time) * 100
+		_progress_bar.value = (1 - storm_timer.time_left / storm_max_value) * 100
