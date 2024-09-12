5a209c31b8201974ddc1ea6c4e793f8a244981e0
chpassos <crlshnrqpassos@gmail.com>
2024-09-11 23:17:01 -0300

    ProgressBar (UI) feedback on storm phase

--- scenes/UI/ui.gd
+++ scenes/UI/ui.gd
@@ -6,4 +6,8 @@ extends CanvasLayer
 @export var _progress_bar: ProgressBar
 
 func _process(_delta):
-	_progress_bar.value = (calm_timer.time_left / calm_timer.wait_time) * 100
+	if calm_timer.time_left > 0:
+		_progress_bar.value = (calm_timer.time_left / calm_timer.wait_time) * 100
+	
+	if storm_timer.time_left > 0:
+		_progress_bar.value = (1 - storm_timer.time_left / storm_timer.wait_time) * 100
