6ac944a322a09c82ee1538fcc35dd93dd29652e7
Alfredo Reis <alf.reismarin@gmail.com>
2024-09-14 17:59:34 -0300

    Set intro as main scene

--- scenes/levels/intro.gd
+++ scenes/levels/intro.gd
@@ -3,3 +3,4 @@ extends Control
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "proverb_fade_out":
 		get_tree().change_scene_to_file("res://scenes/levels/main.tscn")
+		CycleManager.start_cycles()
