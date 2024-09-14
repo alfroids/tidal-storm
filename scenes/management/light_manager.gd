58f77f2fe3ddb57ff1d493b3fd7cca212c2d0a3f
chpassos <crlshnrqpassos@gmail.com>
2024-09-13 22:25:03 -0300

    Fix light number cap problem

--- scenes/management/light_manager.gd
+++ scenes/management/light_manager.gd
@@ -1,22 +1,38 @@
 extends Node
 
-@export var _orb_manager: Node2D
 @export var _sunlight: DirectionalLight2D
+@export var light_scene : PackedScene
+@export var num_lights : int = 3
 
-#@export var _anim: AnimationPlayer
-#
+var lights : Array = [] 
+var square_size : Vector2 = Vector2(3700, 900)
+
 func _ready() -> void:
-	CycleManager.cycle_started.connect(_orbs_visibility) # aqui eu pego o sinal emitido pelo autoload?
+	CycleManager.cycle_started.connect(_orbs_visibility) 
 
 func _orbs_visibility(phase: CycleManager.PHASE) -> void:
 		if phase == CycleManager.PHASE.CALM:
-			_orb_manager.visible = false
+			delete_lights()
 			
 			var tween = get_tree().create_tween()
 			tween.tween_property(_sunlight, "energy", 0.5, 1)
 		
 		elif phase == CycleManager.PHASE.STORM:
-			_orb_manager.visible = true
+			spawn_random_lights()
 			
 			var tween = get_tree().create_tween()
 			tween.tween_property(_sunlight, "energy", 0.15, 1)
+
+func spawn_random_lights():
+	for i in range(num_lights):
+		var light_instance = light_scene.instantiate()
+		var random_x = randf() * square_size.x
+		var random_y = randf() * square_size.y
+		light_instance.position = Vector2(random_x, random_y)
+		add_child(light_instance)
+		lights.append(light_instance)  
+
+func delete_lights():
+	for light in lights:
+		light.queue_free()  
+	lights.clear()  
